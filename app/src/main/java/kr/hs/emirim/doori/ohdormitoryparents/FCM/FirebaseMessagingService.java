59f2972d95e5a6e572ced513576533dcabcff1f6
kimdoori <rlaenfl0228@gmail.com>
2017-10-22 10:55:00 +0900

    notification done

--- app/src/main/java/kr/hs/emirim/doori/ohdormitoryparents/FCM/FirebaseMessagingService.java
+++ app/src/main/java/kr/hs/emirim/doori/ohdormitoryparents/FCM/FirebaseMessagingService.java
@@ -10,11 +10,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
 
 import kr.hs.emirim.doori.ohdormitoryparents.MainActivity;
-import kr.hs.emirim.doori.ohdormitoryparents.R;
+
+import static kr.hs.emirim.doori.ohdormitoryparents.R.mipmap.ic_launcher;
 
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
@@ -24,22 +26,24 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        sendPushNotification(remoteMessage.getData().get("message"));
+        sendPushNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
     }
 
-    private void sendPushNotification(String message) {
-        System.out.println("received message : " + message);
+    private void sendPushNotification(String title, String body) {
+        Log.e("received message : " , title);
+        Log.e("received message : " , body);
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setContentTitle("Push Title ")
-                .setContentText(message)
+                .setContentTitle(title)
+                .setContentText(body)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.notification);
+                .setSmallIcon(ic_launcher);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
